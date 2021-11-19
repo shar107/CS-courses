d6faca4da5c3a263953d21af8206cfb293535a8b
Sharadindu Adhikari <djsharadindu@gmail.com>
2021-11-20 01:43:24 +0530

    .

---
+++ Web mining/zFinal.py
@@ -0,0 +1,22 @@
+#Final.
+
+
+#Q. Write a python program to find the important words from the text using TFIDF.
+"""Use any 5 documents in the course page location(Samples given)
+a) https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-
+867-machine-learning-fall-2006/lecture-notes/lec1.pdf
+b) https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-
+867-machine-learning-fall-2006/lecture-notes/lec2.pdf
+c) https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-
+867-machine-learning-fall-2006/lecture-notes/lec4.pdf
+d) D & E can be taken of your choice"""
+
+
+#Input data:
+
+The given 4 link pdfs and I’ve chosen d) as lec3.pdf (mit.edu) and e) as lec5.pdf (mit.edu)
+
+
+
+#Code:
+
