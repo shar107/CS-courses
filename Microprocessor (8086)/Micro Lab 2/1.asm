2430d675568716b565ca29066f3d5bf95b1c4f81
Sharadindu Adhikari <djsharadindu@gmail.com>
2021-11-29 10:56:00 +0530

    Create 1.asm

---
+++ Microprocessor (8086)/Micro Lab 2/1.asm
@@ -0,0 +1,79 @@
+
+1.	Aim: To convert the given temperature in Celsius scale to Fahrenheit scale. 
+
+Input: I’m inputting 37°.
+
+Algorithm: The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32
+
+Input: I’m inputting 37°.
+
+
+Code:
+
+DATA SEGMENT
+T    DB ?
+RES  DB 10 DUP ('$')
+MSG1 DB "ENTER TEMPERATURE IN CELSIUS (ONLY IN 2 DIGITS) : $"
+MSG2 DB 10,13,"CONVERTED IS FAHRENHEIT (TEMPERATURE) : $"
+DATA ENDS
+CODE SEGMENT
+ASSUME DS:DATA,CS:CODE
+START:
+MOV AX,DATA
+MOV DS,AX
+LEA DX,MSG1
+MOV AH,9
+INT 21H
+MOV AH,1
+INT 21H
+SUB AL,30H
+MOV AH,0
+MOV BL,10
+MUL BL
+MOV BL,AL
+MOV AH,1
+INT 21H
+SUB AL,30H
+MOV AH,0
+ADD AL,BL
+MOV T,AL
+MOV DL,9
+MUL DL
+MOV BL,5
+DIV BL
+MOV AH,0
+ADD AL,32
+LEA SI,RES
+CALL HEX2DEC
+LEA DX,MSG2
+MOV AH,9
+INT 21H
+LEA DX,RES
+MOV AH,9
+INT 21H
+MOV AH,4CH
+INT 21H
+CODE ENDS
+HEX2DEC PROC NEAR
+MOV CX,0
+MOV BX,10
+LOOP1: MOV DX,0
+DIV BX
+ADD DL,30H
+PUSH DX
+INC CX
+CMP AX,9
+JG LOOP1
+ADD AL,30H
+MOV [SI],AL
+LOOP2: POP AX
+INC SI
+MOV [SI],AL
+LOOP LOOP2
+RET
+HEX2DEC ENDP
+END START
+
+
+
+Output: AX=0068 (in degree C to degree F formula=>F=((C*9)/5)+32, here AX or C=37, therefore F=98
